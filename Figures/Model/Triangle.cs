3514a50eaf954a03f93b4955addf2e7302555aef
Ants1ferov <antsiferov.n1k@yandex.ru>
2022-12-20 18:03:00 +0400

    Реализация библиотеки Figures: описание классов под фигуры треугольник, круг

---
+++ Figures/Model/Triangle.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Figures.Model
+{
+    /// <summary>
+    /// Треугольник
+    /// </summary>
+    public class Triangle
+    {
+        /// <summary>
+        /// Конструктор класса "Треугольник"
+        /// </summary>
+        /// <param name="firstSide"> Первая сторона </param>
+        /// <param name="secondSide"> Вторая сторона </param>
+        /// <param name="thirdSide"> Третья сторона </param>
+        /// <exception cref="ArgumentException"> Аргументы не могут принимать нулевые или отрицательные вхождения </exception>
+        public Triangle(int firstSide, int secondSide, int thirdSide) 
+        {
+            #region Блок проверок
+            if (firstSide <= 0)
+            {
+                throw new ArgumentException("Сторона не может быть меньше либо равна нулю", nameof(firstSide));
+            }
+            if (secondSide <= 0)
+            {
+                throw new ArgumentException("Сторона не может быть меньше либо равна нулю", nameof(secondSide));
+            }
+            if (thirdSide <= 0)
+            {
+                throw new ArgumentException("Сторона не может быть меньше либо равна нулю", nameof(thirdSide));
+            }
+            #endregion
+
+            FirstSide = firstSide;
+            SecondSide = secondSide;
+            ThirdSide = thirdSide;
+        }
+
+        #region Свойства
+        /// <summary>
+        /// Сторона треугольника
+        /// </summary>
+        protected int FirstSide { get; set; }
+        /// <summary>
+        /// Сторона треугольника
+        /// </summary>
+        protected int SecondSide { get; set; }
+        /// <summary>
+        /// Сторона треугольника
+        /// </summary>
+        protected int ThirdSide { get; set; }
+        /// <summary>
+        /// Площадь треугольника
+        /// </summary>
+        protected double Square { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Ищет максимальное значение из всех сторон и проверяет на существование данного треугольника
+        /// </summary>
+        /// <returns> Значение true, если треугольник существует, false - в ином случае </returns>
+        public bool CheckingExistenceTriangle()
+        {
+
+            if (FirstSide <= 0 || SecondSide <= 0 || ThirdSide <= 0) return false;
+
+            if (FirstSide > SecondSide && FirstSide > ThirdSide && FirstSide < (SecondSide + ThirdSide))
+            {
+                return true;
+            }
+            else if (SecondSide > FirstSide && SecondSide > ThirdSide && SecondSide < (FirstSide + ThirdSide))
+            {
+                return true;
+            }
+            else if (ThirdSide > FirstSide && ThirdSide > SecondSide && ThirdSide < (FirstSide + SecondSide))
+            {
+                return true;
+            }
+            else if (FirstSide == SecondSide && FirstSide == ThirdSide && SecondSide == ThirdSide)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Находит площадь треугольника
+        /// </summary>
+        /// <returns> Значение площади треугольника </returns>
+        public double FindingSquare()
+        {
+            double perimeter = FirstSide + SecondSide + ThirdSide;
+            return Square = Math.Sqrt(perimeter * (perimeter - FirstSide) * (perimeter - SecondSide) * (perimeter - ThirdSide));
+        }
+    }
+}
---
+++ Figures/Model/Сircle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Figures.Model
+{
+    /// <summary>
+    /// Круг
+    /// </summary>
+    public class Circle
+    {
+        /// <summary>
+        /// Конструктор класса "Круг"
+        /// </summary>
+        /// <param name="radius"> Радиус </param>
+        /// <exception cref="ArgumentException"> Аргументы не могут принимать нулевые или отрицательные вхождения </exception>
+        public Circle(int radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Радиус не может быть меньше либо равен нулю", nameof(radius));
+            }
+
+            Radius = radius;
+        }
+
+        #region Свойства
+        /// <summary>
+        /// Площадь круга
+        /// </summary>
+        public int Square { get; private set; }
+        /// <summary>
+        /// Радиус круга
+        /// </summary>
+        public int Radius { get; private set; }
+        #endregion
+
+        /// <summary>
+        /// Находит площадь круга
+        /// </summary>
+        /// <returns> Значение площади круга </returns>
+        public double FindingSquare()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
